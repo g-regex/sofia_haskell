59d4916c0418849e00a990dec41877b26e4d6c64
Gregor Feierabend <23300469@sun.ac.za>
2022-01-15 21:56:07 +0200

    added some style

--- WebInterface.hs
+++ WebInterface.hs
@@ -16,7 +16,72 @@ mkYesod "App" [parseRoutes|
 
 instance Yesod App where
     defaultLayout widget = do
-        pc <- widgetToPageContent widget
+        pc <- widgetToPageContent $ do
+            widget
+            toWidget [lucius|
+                     body {
+                          font-family: verdana;
+                          background: #181a1b;
+                          color: white;
+                          font-size: 12pt;
+                     }
+                     #mainBox {
+                       border-radius: 25px;
+                       padding: 20px;
+                       margin: 50px;
+                     }
+
+                     .column {
+                         float: left;
+                         width: 50%;
+                         flex: 1;
+                     }
+
+                     .row {
+                         margin: 10px;
+                         display: flex;
+                     }
+
+                     #proof {
+                         border-top-left-radius: 25px;
+                         border-bottom-left-radius: 25px;
+                         background: #3e4446;
+                     }
+
+                     #info {
+                         border-top-right-radius: 25px;
+                         border-bottom-right-radius: 25px;
+                         background: #35393b;
+                     }
+
+                     .inside {
+                         margin: 20px;
+                     }
+
+                     #cmd {
+                       border-radius: 25px;
+                       background: #3e4446;
+                       padding: 20px;
+                       margin: 10px;
+                     }
+
+                     #prompt {
+                       width: 100%;
+                     }
+
+                     input[type=text] {
+                          border: none;
+                          background: #3e4446;
+                          border-bottom: 2px solid darkgrey;
+                          color: white;
+                          font-size: 12pt;
+                     }
+
+                     input[type=text]:focus {
+                          border-bottom: 2px solid grey;
+                          outline: none;
+                     }
+                    |]
         withUrlRenderer
             [hamlet|
                 $doctype 5
@@ -25,7 +90,8 @@ instance Yesod App where
                         <title>#{pageTitle pc}
                         ^{pageHead pc}
                     <body>
-                        ^{pageBody pc}
+                        <div #mainBox>
+                            ^{pageBody pc}
             |]
 
 instance RenderMessage App FormMessage where
@@ -44,34 +110,41 @@ postHomeR = do
     hst <- runInputPost $ iopt textField "history"
     msg <- runInputPost $ iopt textField "message"
     defaultLayout
-        [whamlet|
-            <form method=post action=@{HomeR}>
-                $maybe m <- msg
-                    $if validateCmd $ unpack m
-                        $maybe h <- hst
-                                $forall line <- genProof (unpack h) [unpack m]
-                                    #{line}<br>
-                                <input type=hidden name=history value="#{h};#{m}">
-                        $nothing
-                            #{pack (show $ evalList [unpack m])}<br>
-                            <input type=hidden name=history value="#{m}">
-                    $else
-                        $maybe h <- hst
-                            $forall line <- genProof (unpack h) []
-                                #{line}<br>
-                            <input type=hidden name=history value="#{h}">
-                        <b>Error: Invalid command.</b>
-                $nothing
-                    $maybe h <- hst
-                        $forall line <- genProof (unpack h) []
-                            #{line}<br>
-                        <input type=hidden name=history value="#{h}">
-                    $nothing
-                        Hello! You can start creating a proof.
-                <br><br>
-                <input type=text name=message>
-                <button>Go
-        |]
+     [whamlet|
+     <form method=post action=@{HomeR}>
+         <div .row>
+          <div .column #proof>
+           <div .inside>
+             $maybe m <- msg
+                 $if validateCmd $ unpack m
+                     $maybe h <- hst
+                             $forall line <- genProof (unpack h) [unpack m]
+                                 #{line}<br>
+                             <input type=hidden name=history value="#{h};#{m}">
+                     $nothing
+                         #{pack (show $ evalList [unpack m])}<br>
+                         <input type=hidden name=history value="#{m}">
+                 $else
+                     $maybe h <- hst
+                         $forall line <- genProof (unpack h) []
+                             #{line}<br>
+                         <input type=hidden name=history value="#{h}">
+                     <br><b>Error: Invalid command.</b>
+             $nothing
+                 $maybe h <- hst
+                     $forall line <- genProof (unpack h) []
+                         #{line}<br>
+                     <input type=hidden name=history value="#{h}">
+                 $nothing
+                     Hello! You can start creating a proof.
+             <br>
+          <div .column #info>
+           <div .inside>
+            blabla
+         <div #cmd>
+             <input #prompt type=text name=message
+                placeholder="Type Command ..." autofocus>
+     |]
 
 main :: IO ()
 main = warp 3000 App
